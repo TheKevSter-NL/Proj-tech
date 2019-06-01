7fc881185da1f849b43c91fb15506df3efb489b6
TheKevSter35 <43183768+TheKevSter35@users.noreply.github.com>
2019-06-02 00:13:15 +0200

    Add files via upload

--- mongoose.js
+++ mongoose.js
@@ -6,17 +6,6 @@ const express = require('express');
 const session = require('express-session');
 const app = express();
 
-// app.use(session({
-//     resave: false,
-//     saveUninitialized: true,
-//     host: process.env.DB_HOST,
-//     db_name: process.env.DB_NAME,
-//   }));
-//   app.listen(process.env.PORT);
-//   process.env['DB_HOST'] = 'host';
-//   process.env['MONGO_DB'] = 'database';
-
-
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/datingapp', {useNewUrlParser: true});
 var userSchema = new mongoose.Schema({
--- server.js
+++ server.js
@@ -12,12 +12,10 @@ const mongojs = require('mongojs');
 const db = mongojs('datingapp', ['users']);
 const port = process.env.DB_PORT;
 const mongoosLogin = require('./mongoose');
-const multer  = require('multer');
+const multer = require('multer');
+// const argon2 = require('argon2');
 
-
-
-
-//view Engine
+//view Engines
 app.set('view engine', 'ejs');
 app.set('views', 'view');
 
@@ -38,51 +36,102 @@ app.use(session({
   db_name: process.env.DB_NAME,
   secret: process.env.SESSION_SECRET,
   port: process.env.PORT,
+  maxAge: 24 * 60 * 60,
+  secure: false
+
 }));
 app.listen(process.env.PORT);
-
-
-app.get("/", start);
-app.get("/index", home);
-app.get("/register", register);
-app.get("/uploadprofile", uploadprofile);
-app.get("/inloggen", inloggen);
-app.get("/chats", chats);
-app.get("/profile", profile);
-app.get("/edit", edit);
-app.get("/userprofile", userprofile);
-app.get("/notification", notification);
-app.get("/setting", setting);
-app.get('/log-out', logout);
-
-
 
 // Storage Engine
 const storage = multer.diskStorage({
   destination: './static/uploads/',
-  filename: function(req, file, cb){
-    cb(null,file.fieldname +  path.extname(file.originalname));
-  } 
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + path.extname(file.originalname));
+  }
 });
 // upload
 
-const upload = multer({ storage: storage }).single('profile-pic');
+const upload = multer({
+  storage: storage
+}).single('profile-pic');
 
 app.post('/users/upload', (req, res) => {
   upload(req, res, (err) => {
-    if(err){
-      res.redirect('pages/uploadprofile.ejs', { msg: err});
-     } 
-     else {
-       console.log(req.file);
-       res.redirect('/userprofile');
+    if (err) {
+      res.redirect('pages/uploadprofile.ejs', {
+        msg: err
+      });
+    } else {
+      console.log(req.file);
+      res.redirect('/userprofile');
 
     }
   });
 });
+//Edit function
+app.post('/users/edit', function (req, res) {
+  db.users.update({}, {
+    studenten_nummer: req.body.studenten_nummer,
+    password: req.body.password,
+    first_name: req.body.first_name,
+    opleiding: req.body.opleiding,
+    leeftijd: req.body.leeftijd,
+    overJezelf: req.body.overJezelf
+  });
+  console.log('Profile is aangepast');
+  res.redirect("../uploadprofile");
+});
+//Sign in function
+app.post('/users/signin', function (req, res) {
 
+  studenten_nummer = req.body.studenten_nummer;
+  process.env.PASSWORD = req.body.password;
 
-function logout(req, res, next) {
+  mongoosLogin.findOne({
+    studenten_nummer: studenten_nummer,
+    password: process.env.PASSWORD
+  }, function (err, mongoosLogin) {
+    if (err) {
+      console.log(err);
+      console.log('Error');
+      return res.redirect('/inloggen');
+    }
+
+    if (!mongoosLogin) {
+      console.log('Inloggen mislukt');
+      return res.redirect('/inloggen');
+    }
+
+    req.session.mongoosLogin = mongoosLogin;
+    console.log('Inloggen gelukt');
+    return res.status(200).send, res.redirect('/index');
+  });
+});
+
+//add new user function
+app.post('/users/add', function (req, res) {
+
+  var newUser = {
+    studenten_nummer: req.body.studenten_nummer,
+    password: req.body.password,
+    first_name: req.body.first_name,
+    opleiding: req.body.opleiding,
+    leeftijd: req.body.leeftijd,
+    overJezelf: req.body.overJezelf
+  };
+  console.log('Registeren is gelukt');
+  res.redirect("../uploadprofile");
+  db.users.insert(newUser);
+});
+//Delete function
+app.get('/users/delete', function (req, res) {
+  db.users.remove({});
+  console.log('Account is gewist');
+  res.redirect("../");
+});
+
+//Logout function
+app.get('/log-out', function (req, res, next) {
   req.session.destroy(function (err) {
     if (err) {
       next(err);
@@ -91,21 +140,101 @@ function logout(req, res, next) {
       console.log('Je bent nu uitgelogd');
     }
   });
-}
-function start(req, res) {
+});
+
+//--------PAGES--------------
+
+//START
+app.get("/", function (req, res) {
   res.render('pages/start.ejs', {
     title: "start"
   });
-}
+});
+
+//INDEX (DASHBOARD)  PAGE
+app.get("/index", function (req, res) {
+  if (!req.session.mongoosLogin) {
+    return res.redirect('inloggen'), res.status(401).send();
+  }
+  res.render('pages/index.ejs', {
+    title: "Home",
+    mongoosLogin: req.session.mongoosLogin
+  });
+  console.log(process.env.SESSION_SECRET);
+  return res.status(200).send(process.env.SESSION_SECRET);
+});
+
+//CHATS PAGE
+app.get("/chats", function (req, res) {
+  if (!req.session.mongoosLogin) {
+    return res.status(401).send(),
+      res.redirect('inloggen');
+  }
+  res.render('pages/chat.ejs', {
+    title: "Chat",
+    mongoosLogin: req.session.mongoosLogin
+  });
+  console.log(process.env.SESSION_SECRET);
+  return res.status(200).send(process.env.SESSION_SECRET);
+});
+
+//Profile PAGE
+app.get("/profile", function (req, res) {
+  if (!req.session.mongoosLogin) {
+    return res.status(401).send(),
+      res.redirect('inloggen');
+  }
+  res.render('pages/profile.ejs', {
+    title: "Profile",
+    mongoosLogin: req.session.mongoosLogin
+  });
+  console.log(process.env.SESSION_SECRET);
+  return res.status(200).send(process.env.SESSION_SECRET);
+});
+
+//Notification PAGE
+app.get("/notification", function (req, res) {
+  if (!req.session.mongoosLogin) {
+    return res.status(401).send(),
+      res.redirect('inloggen');
+  }
+  res.render('pages/notification.ejs', {
+    title: "Notification",
+    mongoosLogin: req.session.mongoosLogin
+  });
+  console.log(process.env.SESSION_SECRET);
+  return res.status(200).send(process.env.SESSION_SECRET);
+});
+
+//Setting PAGE
+app.get("/setting", function (req, res) {
+  if (!req.session.mongoosLogin) {
+    return res.status(401).send(),
+      res.redirect('inloggen');
+  }
+  res.render('pages/setting.ejs', {
+    title: "Setting",
+    mongoosLogin: req.session.mongoosLogin
+  });
+  console.log(process.env.SESSION_SECRET);
+  return res.status(200).send(process.env.SESSION_SECRET);
+});
+
+//Register PAGE
+app.get("/register", register);
 
 function register(req, res) {
   db.users.find(function (docs) {
-  res.render('pages/register.ejs', {
-    title: "register",
-    users: docs
+    res.render('pages/register.ejs', {
+      title: "register",
+      users: docs
+    });
   });
-});
 }
+
+//Upload profile PAGE
+app.get("/uploadprofile", uploadprofile);
+
 function uploadprofile(req, res) {
   res.render('pages/uploadprofile.ejs', {
     title: "profile-uploaden",
@@ -114,90 +243,22 @@ function uploadprofile(req, res) {
   });
 }
 
-app.post('/users/add', function (req, res) {
-      var newUser = { 
-        studenten_nummer: req.body.studenten_nummer,
-        password: req.body.password,
-        first_name: req.body.first_name,
-        opleiding: req.body.opleiding,
-        leeftijd: req.body.leeftijd,
-        overJezelf: req.body.overJezelf
-      };
-      console.log('Registeren is gelukt');
-      res.redirect("../uploadprofile");
-      db.users.insert(newUser);
-    }
-);
-
-app.get('/users/delete', function (req, res) {    
-  db.users.remove( {});
-    console.log('Account is gewist');
-    res.redirect("../");
-  }
-);
+//Inlog PAGE
+app.get("/inloggen", inloggen);
 
 function inloggen(req, res) {
   db.users.find(function (docs) {
-  res.render('pages/inloggen.ejs', {
-    title: "inloggen",
-    user: req.session.user,
-    users: docs,
-    mongoosLogin: req.session.mongoosLogin
-  });
-});
-}
-
-app.post('/users/signin', function(req, res) {
-  
-  studenten_nummer= req.body.studenten_nummer;
-  process.env.PASSWORD = req.body.password;
-
-  mongoosLogin.findOne({studenten_nummer: studenten_nummer, password: process.env.PASSWORD}, function(err, mongoosLogin){
-    if(err){
-      console.log(err);
-      console.log('Error');
-      return res.redirect('/inloggen');
-    }
-    if(!mongoosLogin){
-      
-      console.log('Inloggen mislukt');
-      return res.redirect('/inloggen');
-      
-    }
-    req.session.mongoosLogin = mongoosLogin;
-    console.log('Inloggen gelukt');
-    return res.status(200).send, res.redirect('/index');
-    
-  
+    res.render('pages/inloggen.ejs', {
+      title: "inloggen",
+      user: req.session.user,
+      users: docs,
+      mongoosLogin: req.session.mongoosLogin
     });
   });
-
- app.get('/user-check' ,function(req, res){
-  
-  if(!req.session.mongoosLogin) {
-    return res.status(401).send();
-  }
-  req.session.mongoosLogin = mongoosLogin;
-  return  res.status(200).send(process.env.SESSION_SECRET);
-
-});
-
-
-function home(req, res) {
-  res.render('pages/index.ejs', {
-    title: "Home",
-    mongoosLogin: req.session.mongoosLogin
-  });
 }
 
-
-
-function chats(req, res) {
-  res.render('pages/chats.ejs', {
-    title: "chats",
-    mongoosLogin: req.session.mongoosLogin
-  });
-}
+//Edit PAGE
+app.get("/edit", edit);
 
 function edit(req, res) {
   db.users.find(function (docs) {
@@ -206,31 +267,10 @@ function edit(req, res) {
       users: docs,
       mongoosLogin: req.session.mongoosLogin
     });
-});
-}
-app.post('/users/edit', function (req, res) {
-  
-    db.users.update( {}, { 
-      studenten_nummer: req.body.studenten_nummer,
-        password: req.body.password,
-        first_name: req.body.first_name,
-        opleiding: req.body.opleiding,
-        leeftijd: req.body.leeftijd,
-        overJezelf: req.body.overJezelf
-    } );
-    console.log('Profile is aangepast');
-    res.redirect("../uploadprofile");
-    
-  }
-);
-
-function profile(req, res) {
-  res.render('pages/profile.ejs', {
-    title: "profile",
-    mongoosLogin: req.session.mongoosLogin
   });
 }
-
+//Userprofile PAGE
+app.get("/userprofile", userprofile);
 function userprofile(req, res) {
   db.users.find(function (err, docs) {
     console.log(docs);
@@ -240,28 +280,22 @@ function userprofile(req, res) {
       mongoosLogin: req.session.mongoosLogin
     });
   });
-
 }
 
+//-------------------------
 
-function notification(req, res) {
-  res.render('pages/notification.ejs', {
-    title: "notification",
-    mongoosLogin: req.session.mongoosLogin
-  });
-}
-
-function setting(req, res) {
-  res.render('pages/setting.ejs', {
-    title: "setting",
-    mongoosLogin: req.session.mongoosLogin
-  });
-}
 app.use(function (req, res, next) {
   res.status(404).render('not-found');
 });
 
 app.listen(port, function () {
-  // eslint-disable-next-line no-console
-  console.log(`Server started on port 3000`);
+  console.log(`Server is online ` + port);
 });
+
+// app.get('/user-check' ,function(req, res){
+//   if(!req.session.mongoosLogin) {
+//     return res.status(401).send();
+//   }
+//   console.log(process.env.SESSION_SECRET);
+//   return res.status(200).send(process.env.SESSION_SECRET);
+// });

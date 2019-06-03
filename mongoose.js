860fd4da05927e726b53bd63cb2436fd73471539
TheKevSter-NL <k.curi@hotmail.com>
2019-06-03 22:19:37 +0200

    heroku

--- mongoose.js
+++ mongoose.js
@@ -7,7 +7,7 @@ const session = require('express-session');
 const app = express();
 
 var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://kevster:<password>@dattingapp-fr6ws.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true}, {useMongoClient: true});
+mongoose.connect(process.env.DATABASE_URL || 'postgres://dfrucmmpjlpaew:1e82d0ab56f1e51eff86da62e81d4f208e97868f93bef42a74af06baf6e15add@ec2-54-75-245-196.eu-west-1.compute.amazonaws.com:5432/d44jfmo3nklk3', {useNewUrlParser: true}, {useMongoClient: true});
 // mongoose.connect('mongodb://localhost/datingapp', {useNewUrlParser: true});
 var userSchema = new mongoose.Schema({
     studenten_nummer: {type: String, unique: true},

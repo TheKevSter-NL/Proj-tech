6852dbb7a6cc8991089b14befd6229ec063e4f12
TheKevSter-NL <k.curi@hotmail.com>
2019-06-04 00:04:42 +0200

    Final versie

--- mongoose.js
+++ mongoose.js
@@ -7,8 +7,8 @@ const session = require('express-session');
 const app = express();
 
 var mongoose = require('mongoose');
-mongoose.connect(process.env.DB_URL || 'mongodb+srv://kevster:Start123@dattingapp-fr6ws.mongodb.net/test?retryWrites=true&w=majority'), {useNewUrlParser: true};
-// mongoose.connect('mongodb://localhost/datingapp', {useNewUrlParser: true});
+// mongoose.connect(process.env.DB_URL || 'mongodb+srv://kevster:Start123@dattingapp-fr6ws.mongodb.net/test?retryWrites=true&w=majority'), {useNewUrlParser: true};
+mongoose.connect('mongodb://localhost/datingapp', {useNewUrlParser: true});
 var userSchema = new mongoose.Schema({
     studenten_nummer: {type: String, unique: true},
     password: {type: String}
